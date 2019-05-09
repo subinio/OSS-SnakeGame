1957777ba69b0363269ae0266c2f05c422d420a2
subinio <subinjin22@gmail.com>
2019-05-09 23:59:31 +0900

    modified header file

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "conio.h"
-#include "windows.h"
-#include "time.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
 
 
 // GLOBAL CONSTANTS
