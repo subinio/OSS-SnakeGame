34d6688cc2a12b54ccfca68337f0ce59ac318d62
subinio <subinjin22@gmail.com>
2019-05-29 21:33:07 +0900

    꼬리 생성 기능 추가
    1. 뱀을 이중연결리스트로 구현
       뱀을 이중연결리스트로 구현하기 위해서 뱀의 구조체에 pre와 next의 포인터 필드 생성.
    
    2. Init 함수 InitInfo로 함수명 변경
       함수명을 목적에 맞게 조금더 구체화 시켜주었음.
    
    3. InitSnake 함수생성
       Snake의 정보들을 초기화해주는 함수 생성.
    
    4. Snake * Head 와 Snake * Tail 의 구초제 포인터 생성
       Head포인터는 뱀의 머리를, Tail 포인터는 뱀의 꼬리를 가르킴.
    
    5. InitMap함수 삭제
       InitMap의 함수가 함수의 역할과 이름미 맞지 않았고, 다른 함수와 중복되는 내용이므로 삭제 후 내용이 같은 함수로 대체
    
    6. DrawSnake, DelSnake 함수 수정
       뱀을 이중연결리스트로 구현했기 때문에 기존의 DrawSnake 함수와
    DelSnake함수를 while 문을 사용하여 연결리스트 전체를 줄력하도록 설정
    
    7. AddTail 함수 생성
       먹이를 먹으면 꼬리가 생성되는 함수 생성. 이 꼬리는 이중연결리스트로
    AddTail 함수가 진행되면 하나의 꼬리를 나타내는 노드 한개가 Tail
    포인터 뒤에 연결됨.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include <stdbool.h>
 
-/* Keyboard Input*/
+/* Keyboard Input */
 #define UP 72
 #define DOWN 80
 #define RIGHT 77
@@ -14,11 +14,11 @@
 #define ENTER 13
 #define ESC 27
 
-/* Size of GameBoard*/
+/* Size of GameBoard */
 #define BOARD_WIDTH 45
 #define BOARD_HEIGHT 23
 
-/*Game Border*/
+/* Game Border */
 #define TOP_BORDER 5
 #define BOTTOM_BORDER 27
 #define LEFT_BORDER 5
@@ -26,56 +26,56 @@
 
 typedef struct Snake {
 	COORD pos;
+	int dir;
+	struct Snake * pre;
+	struct Snake * next;
 }Snake;
 
 typedef struct Food {
 	COORD pos;
 }Food;
 
-Snake snake;
+Snake * Head;
+Snake * Tail;
+
 Food food;
 
-/*Functions Declaration*/
+/* Functions Declaration */
 static int wait;
 static int lives;
-static int direction;
 static int score;
 static int speed;
 
 
 static char MAP[23][45] =
 {
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-
-{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0 ,0,0,0,0,0, 0,0,0,0,0, 0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-
-{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-
-{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-
-{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
 };
 
-void Init(void);
-void InitMap(void);
+void InitInfo(void);
+void InitSnake(void);
 void DrawSnake(void);
 void DelSnake(void);
 void Display(void);
@@ -89,28 +89,34 @@ void DrawMap(void);
 void DrawFood(void);
 void SetCurrentCursorPos(int x, int y);
 void RemoveCursor(void);
+void RelocateFood(void);
+void RelocateSnake(void);
 
 int main(void)
 {
 	system("title Snake Game(KSNB)");
 	system("mode con cols=112 lines=33");
+	
 	RemoveCursor();
 
-	srand(time(0));
+	srand((unsigned int)time(0)); 
 
-	Init();
+	InitInfo();
+	InitSnake();
 
 	DrawTitle();
 	DrawMap();
-	InitMap();
-	DrawFood();
-	while (lives) {
+	
+	RelocateFood();
+
+	while (lives) 
+	{
 		Display();
 		CheckSnakeDie(MoveSnake());
 		GetInput();
 		EatFood();
-		SetCurrentCursorPos(6 * 2, 28);
 	}
+	
 	return 0;
 }
 
@@ -128,52 +134,304 @@ void RemoveCursor(void)
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
 }
 
-void Init(void)
+void InitInfo(void)
 {
-	wait = 100;
-	direction = DOWN;
+	wait = 150;
 	lives = 100;
 	score = 0;
 	speed = 1;
 }
 
-void InitMap(void)
+void InitSnake(void)
 {
-	snake.pos.X = (rand() % (BOARD_WIDTH - 2)) + LEFT_BORDER + 1;
-	snake.pos.Y = (rand() % (BOARD_HEIGHT - 2)) + TOP_BORDER + 1;
+	Head = (Snake *)malloc(sizeof(Snake));
+	Head->pre = NULL;
+	Head->next = NULL;
 
-	food.pos.X = (rand() % (BOARD_WIDTH - 2)) + LEFT_BORDER + 1;
-	food.pos.Y = (rand() % (BOARD_HEIGHT - 2)) + TOP_BORDER + 1;
+	Head->pos.X = (rand() % (BOARD_WIDTH - 2)) + LEFT_BORDER + 1;
+	Head->pos.Y = (rand() % (BOARD_HEIGHT - 2)) + TOP_BORDER + 1;
+	Head->dir = DOWN;
+
+	Tail = Head;
 }
 
 void DrawSnake(void)
 {
-	SetCurrentCursorPos(snake.pos.X,snake.pos.Y);
-	printf("%d",snake.pos.X);
+	Snake * temp = Head;
+
+	while (temp != NULL)
+	{
+		SetCurrentCursorPos(temp->pos.X, temp->pos.Y);
+		printf("¿");
+
+		temp = temp->next;
+	}
 }
 
 void DelSnake(void)
 {
-	SetCurrentCursorPos(snake.pos.X, snake.pos.Y);
-	printf("  ");
+	Snake * temp = Head;
+
+	while (temp != NULL)
+	{
+		SetCurrentCursorPos(temp->pos.X, temp->pos.Y);
+		printf("  ");
+
+		temp = temp->next;
+	}
 }
 
 void DrawFood(void)
 {
 	SetCurrentCursorPos(food.pos.X, food.pos.Y);
-	printf("¡Ú");
+	printf("¿");
 }
 
 void DrawMap(void)
 {
-	int i = 0, j = 0;
-	
+	int i, j = 0;
+
 	SetCurrentCursorPos(0, 5);
-	for (i = 0; i < BOARD_HEIGHT; i++) {
+	
+	for (i = 0; i < BOARD_HEIGHT; i++) 
+	{
 		printf("          ");
-		for (j = 0; j < BOARD_WIDTH; j++) {
+		for (j = 0; j < BOARD_WIDTH; j++) 
+		{
 			if (MAP[i][j] == 1)
-				printf("¡á");
+				printf("¿");
+		
 			else
 				printf("  ");
 		}
+		printf("\n");
+	}
+}
+
+void DrawTitle(void)
+{
+	SetCurrentCursorPos(26, 2);
+	printf("Snake Game");
+}
+
+void DrawScore(void)
+{
+	SetCurrentCursorPos(46, 1);
+	printf("Score: %d", score);
+	SetCurrentCursorPos(46, 2);
+	printf("Lives: %d", lives);
+	SetCurrentCursorPos(46, 3);
+	printf("Speed: %d", speed);
+}
+
+void Display(void)
+{
+	DrawScore();
+
+	DrawSnake();
+
+	Sleep(wait);
+
+	DelSnake();
+}
+
+bool IsCollisionTopBorder(void) 
+{
+	return (Head->pos.Y <= TOP_BORDER);
+}
+
+bool IsCollisionBottomBorder(void) 
+{
+	return (Head->pos.Y >= BOTTOM_BORDER);
+}
+
+bool IsCollisionLeftBorder(void) 
+{
+	return (Head->pos.X <= LEFT_BORDER);
+}
+
+bool IsCollisionRightBorder(void) 
+{
+	return (Head->pos.X >= RIGHT_BORDER);
+}
+
+int MoveSnake(void)
+{
+	Snake * temp = Tail;
+	while (temp != Head) {
+		temp->pos.X = temp->pre->pos.X;
+		temp->pos.Y = temp->pre->pos.Y;
+		temp->dir = temp->pre->dir;
+		
+		temp = temp->pre;
+	}
+	switch (Head->dir)
+	{
+	case UP:
+		Head->pos.Y--;
+		if (IsCollisionTopBorder()) 
+		{
+			return 0;
+		}
+		else 
+		{
+			return 1;
+		}
+		break;
+	
+	case DOWN:
+		Head->pos.Y++;
+		if (IsCollisionBottomBorder()) 
+		{
+			return 0;
+		}
+		else 
+		{
+			return 1;
+		}
+		break;
+	
+	case LEFT:
+		Head->pos.X--;
+		if (IsCollisionLeftBorder()) 
+		{
+			return 0;
+		}
+		else 
+		{
+			return 1;
+		}
+		break;
+	
+	case RIGHT:
+		Head->pos.X++;
+		if (IsCollisionRightBorder()) 
+		{
+			return 0;
+		}
+		else 
+		{
+			return 1;
+		}
+		break;
+	
+	default:
+		return 1;
+	
+	}
+}
+
+void GetInput(void)
+{
+	if (_kbhit()) {
+		Head->dir = _getch();
+		MoveSnake(Head->dir);
+	}
+}
+
+void RelocateFood(void) 
+{
+	food.pos.X = (rand() % (BOARD_WIDTH - 2)) + LEFT_BORDER + 1;
+	food.pos.Y = (rand() % (BOARD_HEIGHT - 2)) + TOP_BORDER + 1;
+
+	DrawFood();
+}
+
+void RelocateSnake(void) 
+{
+	Head->pos.X = (rand() % (BOARD_WIDTH - 2)) + LEFT_BORDER + 1;
+	Head->pos.Y = (rand() % (BOARD_HEIGHT - 2)) + TOP_BORDER + 1;
+}
+
+void AddTail(void) 
+{
+
+	Snake * new = (Snake *)malloc(sizeof(Snake));
+	
+	if (Tail == NULL) {
+		
+		new->dir = Head->dir;
+		new->pos.X = Head->pos.X;
+		new->pos.Y = Head->pos.Y + 1;
+		new->pre = Head;
+		Tail->next = NULL;
+		Head->next = Tail;
+		Tail = new;
+		return;
+	}
+
+	switch (Tail->dir)
+	{
+	case UP:
+		new->pos.X = Tail->pos.X;
+		new->pos.Y = Tail->pos.Y - 1;
+		new->dir = Tail->dir;
+		break;
+
+	case DOWN:
+		new->pos.X = Tail->pos.X;
+		new->pos.Y = Tail->pos.Y + 1;
+		new->dir = Tail->dir;
+		break;
+
+	case LEFT:
+		new->pos.X = Tail->pos.X + 1;
+		new->pos.Y = Tail->pos.Y;
+		new->dir = Tail->dir;
+		break;
+
+	case RIGHT:
+		new->pos.X = Tail->pos.X - 1;
+		new->pos.Y = Tail->pos.Y;
+		new->dir = Tail->dir;
+		break;
+
+	default:
+		break;
+	}
+	
+	Tail->next = new;
+	new->pre = Tail;
+	new->next = NULL;
+	Tail = new;
+}
+
+void EatFood(void)
+{
+	if (Head->pos.Y == food.pos.Y && Head->pos.X == food.pos.X)
+	{
+		score++;
+
+		AddTail();
+		RelocateFood();
+
+		if (score == 5)
+		{
+			wait = 100;
+			speed++;
+		}
+		else if (score == 7)
+		{
+			wait = 80;
+			speed++;
+		}
+		else if (score == 9)
+		{
+			wait = 50;
+			speed++;
+		}
+
+	}
+
+}
+
+
+void CheckSnakeDie(int moving)
+{
+	if (!moving)
+	{
+		lives--;
+
+		RelocateSnake();
+	}
+}
